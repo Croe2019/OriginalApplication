164f9ae45e2b48e0e3b23d8aa6ce503575e00e9d
Croe2019 <sac77qbc@gmail.com>
2020-12-16 21:09:35 +0900

    詳細画面複数枚画像表示

--- OriginalApplication/Controller/DetailViewController.swift
+++ OriginalApplication/Controller/DetailViewController.swift
@@ -10,57 +10,100 @@ import UIKit
 import Firebase
 import FSPagerView
 
-class DetailViewController: UIViewController, FSPagerViewDelegate, FSPagerViewDataSource {
+class DetailViewController: UIViewController, UIScrollViewDelegate {
     
     @IBOutlet weak var memoTitle: UILabel!
     @IBOutlet weak var memoTextBody: UILabel!
     @IBOutlet weak var memoImageView: UIImageView!
+    @IBOutlet weak var memoScrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
     
     var memoTextTitleString = String()
     var memoTextBodyString = String()
     var memoImageString = String()
     // テストコード データのautoIDの番号を取得する変数
     var indexNumber = Int()
-    
     var memoArray = [Memo]()
     
-    @IBOutlet weak var pagerView: FSPagerView!{
-        didSet{
-            
-            self.pagerView.register(FSPagerViewCell.self, forCellWithReuseIdentifier: "cell")
-                        self.pagerView.itemSize = FSPagerView.automaticSize
-        }
-    }
+    // ページ数
+    var pegeCount:Int!
+    // ScrollScreenの高さ
+    var scrollScreenHeight:CGFloat!
+    // ScrollScreenの幅
+    var scrollScreenWidth:CGFloat!
     
-    
-    @IBOutlet weak var pageControl: FSPageControl!{
-        
-        didSet{
-            
-            self.pageControl.numberOfPages = self.memoArray.count
-                        self.pageControl.contentHorizontalAlignment = .right
-                        self.pageControl.contentInsets = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
-        }
-    }
+    var imageWidth:CGFloat!
+    var imageHeight:CGFloat!
+    var screenSize:CGRect!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         memoTitle.text = memoTextTitleString
         memoTextBody.text = memoTextBodyString
-        // pagerView生成
-//        let pagerView = FSPagerView()
-//        pagerView.frame = CGRect(x: 46, y: 120, width: 100, height: 100)
-//        pagerView.delegate = self
-//        pagerView.dataSource = self
-//        pagerView.register(FSPagerViewCell.self, forCellWithReuseIdentifier: "cell")
-//        self.view.addSubview(pagerView)
-//        let pageControl = FSPageControl()
-//        pageControl.frame = CGRect(x: 46, y: 120, width: 100, height: 100)
-//        self.view.addSubview(pageControl)
         memoImageView.sd_setImage(with: URL(string:memoImageString), completed: nil)
         
+        
+        screenSize = UIScreen.main.bounds
+        // ページスクロールとするためにページ幅を合わせる
+        scrollScreenWidth = screenSize.width
+        let imageTop:UIImage = UIImage(named: memoImageString)!
+        pegeCount = memoArray.count
+        
+        imageWidth = imageTop.size.width
+        imageHeight = imageTop.size.height
+        scrollScreenHeight = scrollScreenWidth * imageHeight/imageWidth
+        
+        for i in 0 ..< pegeCount {
+            // UIImageViewのインスタンス
+            let image:UIImage = UIImage(named:memoArray[i].GetImageString())!
+            let imageView = UIImageView(image:image)
+                    
+            var rect:CGRect = imageView.frame
+            rect.size.height = scrollScreenHeight
+            rect.size.width = scrollScreenWidth
+         
+            imageView.frame = rect
+            imageView.tag = i + 1
+                    
+            // UIScrollViewのインスタンスに画像を貼付ける
+            self.memoScrollView.addSubview(imageView)
+                    
+        }
+        setupScrollImages()
     }
+    
+    func setupScrollImages(){
+           
+           // DBにある画像表示 ※現在は画像が1枚しかない状態なので、後で配列にする
+           let image:UIImage = UIImage(named:memoImageString)!
+           var imgView = UIImageView(image:image)
+           var subviews:Array = memoScrollView.subviews
+           
+           // 描画開始の x,y 位置
+           var px:CGFloat = 0.0
+           let py:CGFloat = (screenSize.height - scrollScreenHeight)/2
+           
+           // verticalScrollIndicatorとhorizontalScrollIndicatorが
+           // デフォルトで入っているので2から始める
+           for i in 2 ..< subviews.count {
+               imgView = subviews[i] as! UIImageView
+               if (imgView.isKind(of: UIImageView.self) && imgView.tag > 0){
+                   
+                   var viewFrame:CGRect = imgView.frame
+                   viewFrame.origin = CGPoint(x: px, y: py)
+                   imgView.frame = viewFrame
+                   
+                   px += (scrollScreenWidth)
+                   
+               }
+           }
+           // UIScrollViewのコンテンツサイズを画像のtotalサイズに合わせる
+           let nWidth:CGFloat = scrollScreenWidth * CGFloat(pegeCount)
+           memoScrollView.contentSize = CGSize(width: nWidth, height: scrollScreenHeight)
+           
+    }
+    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
@@ -77,38 +120,4 @@ class DetailViewController: UIViewController, FSPagerViewDelegate, FSPagerViewDa
         }
     }
     
-    
-    // ここにメモごとに保存してある画像の数を返せば良いでしょうか？
-    // その際はメモの配列を作る必要があるでしょうか？
-    // FSPagerView DataSource
-    func numberOfItems(in pagerView: FSPagerView) -> Int {
-        
-        return memoArray.count
-    }
-    
-    // 画像を表示するときはcellを使う必要があるのでしょうか？
-    func pagerView(_ pagerView: FSPagerView, cellForItemAt index: Int) -> FSPagerViewCell {
-        
-        let cell = pagerView.dequeueReusableCell(withReuseIdentifier: "cell", at: index)
-        // メモの画像を入れる
-        cell.imageView?.image = UIImage(named: memoImageString)
-        
-        return cell
-    }
-    
-    // FSPagerView Delegate
-    func pagerView(_ pagerView: FSPagerView, didSelectItemAt index: Int) {
-        pagerView.deselectItem(at: index, animated: true)
-        pagerView.scrollToItem(at: index, animated: true)
-    }
-    
-    func pagerViewWillEndDragging(_ pagerView: FSPagerView, targetIndex: Int) {
-        
-        self.pageControl.currentPage = targetIndex
-    }
-    
-    func pagerViewDidEndScrollAnimation(_ pagerView: FSPagerView) {
-        
-        self.pageControl.currentPage = pagerView.currentIndex
-    }
 }
